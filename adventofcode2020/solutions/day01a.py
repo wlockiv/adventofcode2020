7cffd3c244cf15ac67065d5c7206224616cf8b24
wlockiv <walker.b.lockard@gmail.com>
2020-12-01 08:42:01 -0600

    day 1 optimizations

--- adventofcode2020/solutions/day01a.py
+++ adventofcode2020/solutions/day01a.py
@@ -2,17 +2,17 @@ with open('../../inputs/day01a.txt') as input_file:
     input_list = input_file.read().splitlines()
 
 # Sort the input array
-input_list = [int(i) for i in input_list]
+input_list = set([int(i) for i in input_list])
 
 sum_equals_2020 = (0, 0)
 it = 0
 
-for idx, i in enumerate(input_list):
-    for j in input_list[idx + 1:]:
-        it += 1
-        if i + j == 2020:
-            sum_equals_2020 = (i, j)
-            break
+for i in input_list:
+    it += 1
+    difference = 2020 - i
+    if difference in input_list:
+        sum_equals_2020 = (i, difference)
+        break
 
 print('Iterations', it)
 print('The following numbers sum to 2020:', sum_equals_2020)
--- adventofcode2020/solutions/day01b.py
+++ adventofcode2020/solutions/day01b.py
@@ -2,18 +2,18 @@ with open('../../inputs/day01a.txt') as input_file:
     input_list = input_file.read().splitlines()
 
 # Sort the input array
-input_list = [int(i) for i in input_list]
+input_list = set([int(i) for i in input_list])
 
 sum_equals_2020 = (0, 0, 0)
 it = 0
 
-for idx, i in enumerate(input_list):
-    for j in input_list[idx + 1:]:
-        for k in input_list[idx + 2:]:
-            it += 1
-            if sum((i, j, k)) == 2020:
-                sum_equals_2020 = (i, j, k)
-                break
+for i in input_list:
+    for j in input_list:
+        it += 1
+        difference = 2020 - i - j
+        if difference in input_list:
+            sum_equals_2020 = (i, j, difference)
+            break
 
 print('Iterations', it)
 print('The following numbers sum to 2020:', sum_equals_2020)
