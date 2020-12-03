85b687b94b8aa09162a10533610f06bd33088665
wlockiv <walker.b.lockard@gmail.com>
2020-12-02 21:26:04 -0600

    another exception

--- adventofcode2020/cli.py
+++ adventofcode2020/cli.py
@@ -20,22 +20,25 @@ def cli(ctx, day, part):
     try:
         solution = import_module(
             f'.solutions.day{str(day).zfill(2)}{part}',
-            f'adventofcode2020')
+            f'adventofcode2020').main
 
         if path.isfile(f'./inputs/{challenge}.txt'):
             with open(f'./inputs/{challenge}.txt') as input_file:
-                solution.main(input_file)
+                solution(input_file)
         elif path.isfile(f'./inputs/{challenge[:-1]}a.txt') and part == 'b':
             click.echo('Could not find an input file for part "b". Using part "a".')
             with open(f'./inputs/{challenge[:-1]}a.txt') as input_file:
-                solution.main(input_file)
+                solution(input_file)
         else:
             raise FileNotFoundError(f'Could not find the expected input file: {challenge}.txt')
 
     except ModuleNotFoundError as e:
-        click.echo('The module for this day does not exist.')
+        click.echo('Error: The module for this day does not exist.')
         click.echo(f'Make sure a module named {challenge}.py has been add to solutions.')
         ctx.abort()
     except FileNotFoundError as e:
         click.echo(e)
         ctx.abort()
+    except AttributeError as e:
+        click.echo(
+            'Error: A valid solution module must have a main() function that takes a file object as a parameter.')
